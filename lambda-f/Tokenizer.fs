71c040405e34b2860fb22dd6ce52beb1314a23bd
Babu Thomas <babuthomas138@gmail.com>
2017-09-23 05:02:46 +0530

    Add tokenizer

---
+++ lambda-f/Tokenizer.fs
@@ -0,0 +1,22 @@
+﻿module Tokenizer
+
+type Token =
+    | LParen
+    | RParen
+    | Lambda
+    | Dot
+    | Variable of char
+
+let alphabet = List.ofSeq "abcdefghijklmnopqrstuvwxyz"
+
+let rec tokenize (text:char list) =
+    match text with
+        | [] -> []
+        | '('::rest -> LParen::tokenize rest
+        | ')'::rest -> RParen::tokenize rest
+        | '.'::rest -> Dot::tokenize rest
+        | '\\'::rest -> Lambda::tokenize rest
+        | c::rest ->
+            (if List.contains c alphabet
+            then [Variable c]
+            else []) @ tokenize rest
